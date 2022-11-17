9195a64b701e18a5fa7ae565da154428038065fa
Mateusz <mateusz.rosiak1995@gmail.com>
2022-11-17 12:07:23 +0100

    update db config for heroku app

--- src/config/config.php
+++ src/config/config.php
@@ -3,8 +3,8 @@
 namespace App;
 
 return [
-    "host" => "db-notes-project",
-    "username" => "notesAdmin",
-    "password" => "password123",
-    "dbname" => "notes"
+    "host" => "eu-cdbr-west-03.cleardb.net",
+    "username" => "b609bf1e09e205",
+    "password" => "9c56fb02",
+    "dbname" => "heroku_edc852c9e2897fc"
 ];
